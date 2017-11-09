ee6ad558a0d300352263bcb5383499a3267680ee
Ye Tian <ye.tian.stanford@gmail.com>
2017-11-09 08:22:10 +0000

    completed multiprocess prepare_scannet

--- rcnn/dataset/prepare_scannet.py
+++ rcnn/dataset/prepare_scannet.py
@@ -1,5 +1,5 @@
 """
-Prepare bottles Database
+Prepare scannet Database
 """
 
 from __future__ import absolute_import
@@ -25,6 +25,18 @@ from skimage import io
 from multiprocessing import Pool
 
 
+def mask_imsave(filename, im):
+    # used to save instance labels
+    im = im.astype(np.uint32)
+    io.imsave(filename, im)
+
+def check_save(filename, img, method=pil_imsave):
+    try:
+        method(filename, img)
+    except IOError:
+        os.makedirs(os.path.dirname(img_full_path))
+        method(filename, img)
+
 
 def get_labels(out_s):
     semantic_id_bottles = 1
@@ -47,24 +59,77 @@ def encode_classid(mask):
     mask[mask > 0] += 1000
     return mask
 
-def pil_imsave(filename, im):
-    im = im.astype(np.uint32)
-    '''
-    if (im.shape) == 2:
-        result = Image.fromarray(im, 'I')#.astype(np.int32)
-    else:
-        result = Image.fromarray(im, 'RGB')#.astype(np.int32)
-    result.save('out.bmp')
-    plt.imshow(im)
-    plt.savefig(filename)
-    '''
-    io.imsave(filename, im)
+blensor_result_path = '/mnt/disks/scannetdata/scannet'
+
+def get_paths(i):
+    ins_path = os.path.join(blensor_result_path, '%s/out' % i)
+    ins_ = glob(os.path.join(ins_path, '*.color.jpg'))
+    inst_ids_path = os.path.join(blensor_result_path, '%s/instance-filt' % i)
+    inst_ids_ = glob(os.path.join(inst_ids_path, '*.png'))
+    sem_ids_path = os.path.join(blensor_result_path, '%s/label-filt' % i)
+    sem_ids_ = glob(os.path.join(sem_ids_path, '*.png'))
+    return ins_, inst_ids_, sem_ids_
+
+
+def init_pool(l):
+    global idx
+    idx = 0
+    global lock
+    lock = l
+
+def worker(i, spl, img_dir, gt_dir, files):
+    # i is like scene0034_01, sceneid_roomNb
+    ins_path = os.path.join(blensor_result_path, '%s/out' % i)
+    inst_ids_path = os.path.join(blensor_result_path, '%s/instance-filt' % i)
+    sem_ids_path = os.path.join(blensor_result_path, '%s/label-filt' % i)
+    ins_ = glob(os.path.join(ins_path, '*.color.jpg')) # .../out/frame-000000.color.jpg, frame_id = ss[-16:-10]
+    #inst_ids_ = glob(os.path.join(inst_ids_path, '*.png'))
+    #sem_ids_ = glob(os.path.join(sem_ids_path, '*.png'))
+
+    for in_ in ins_:
+        fid = int(ins[-16:-10])
+        inst_id_ = os.path.join(inst_ids_path, "%d.png" % fid)
+        sem_id_ = os.path.join(sem_ids_path, "%d.png" % fid)
+        if not (os.path.exists(inst_id_) and os.path.exists(sem_id_)):
+            continue
+        # 'house' , 'roomNb' , 'frameNb' , 'type' , 'type2' , 'ext'
+        img_path = "%s/%s/%s/%s_leftImg8bit.tiff" % (img_dir, spl, i, i)
+        seg_path = "%s/%s/%d/%d_000000_000000_gtFine_instanceIds.png" % (gt_dir, spl, i, i)
+        lock.acquire()
+        line = "%d\t%s\t%s\n" % (idx, img_path, seg_path.replace('instanceIds', 'labelTrainIds'))
+        idx += 1
+        files[spl].write(line)
+        lock.release()
+        print(line)
+
+        ########## dump images #################
+        '''
+        Need to change:
+
+        scannet: 
+        in_, inst_id_, sem_id_ are path to input image, instance id map, semantic id map correspondingly
+        im_inp is load from in_, can be simply copied if shapes match, otherwise need to be resized
+        inst_mask is instance id labels loaded from inst_id_
+        sem_mask is sematice id labels loaded from sem_id_
+        mask[i, j] = sem_mask[i, j] * 1000 + inst_mask[i, j]
+        '''
+        im_inp = read_pgm_xyz(inp)
+        im_centroids = load_seg(outp)
+        mask = get_labels(im_centroids)
+        mask = encode_classid(mask)
+        '''
+        Need to change:
+        '''
+        img_full_path = '%s/%s' % (root_path, img_path)
+        seg_full_path = '%s/%s' % (root_path, seg_path)
+        check_save(img_full_path, im_inp, io.imsave)
+        check_save(seg_full_path, mask, mask_imsave) 
+        ########## dump images #################
 
 def main():
-    n_threads = 50
+    n_proc = 50
     #blensor_result_path = args.br_path
-    blensor_result_path = '/mnt/disks/scannetdata/scannet'
-    root_path = '/mnt/disks/scannetdata/maskrcnn'
+    root_path = 'data/scannet'
     gt_dir = "gtFine"  
     img_dir ="leftImg8bit"
     imglists = "imglists"  
@@ -81,65 +146,24 @@ def main():
             check_mkdir(os.path.join(d, spl))
     ids_ = os.listdir(blensor_result_path) # eg. scene0041_01
 
-    ins, inst_ids, sem_ids = [], [], []
-    num_imgs = 0
-    pool = Pool(n_threads)
-    for i in ids_:
-    #for i, data in enumerate(pool.imap(rotation_depth_map, ids_)):
-        ins_path = os.path.join(blensor_result_path, '%s/out' % i)
-        ins_ = glob(os.path.join(ins_path, '*.color.jpg'))
-        inst_ids_path = os.path.join(blensor_result_path, '%s/instance-filt' % i)
-        inst_ids_ = glob(os.path.join(inst_ids_path, '*.png'))
-        sem_ids_path = os.path.join(blensor_result_path, '%s/label-filt' % i)
-        sem_ids_ = glob(os.path.join(sem_ids_path, '*.png'))
-        #print(ins_path, inst_ids_path, sem_ids_path)
-        assert len(ins_) == len(inst_ids_) == len(sem_ids_)
-        ins += ins_
-        inst_ids += inst_ids_
-        sem_ids += sem_ids_
-        num_imgs += len(ins_)
-        print(len(ins_))
-    #num_imgs = len(ins)
-    print(num_imgs, '-' * 80)
-    exit()
+    l = multiprocessing.Lock()
+    pool = Pool(n_proc, initializer=init_pool, initargs=(l,))
+
+
 
     files = {}
     for spl in splits_dict.values():
         files[spl] = open("%s/%s/%s.lst" % (root_path, imglists, spl), 'w')
-    for i, (idx, inp, outp) in enumerate(zip(ids, ins, outs)):
-        spl = splits_dict[np.sum((splits - (i * 1.0 / num_imgs)) <= 0)]
-        # 'city' , 'sequenceNb' , 'frameNb' , 'type' , 'type2' , 'ext'
-        # 'house' , 'roomNb' , 'frameNb' , 'type' , 'type2' , 'ext'
-        img_path = "%s/%s/%d/%d_000000_000000_gtFine_instanceIds.png" % (img_dir, spl, idx, idx)
-        seg_path = "%s/%s/%d/%d_000000_000000_gtFine_instanceIds.png" % (gt_dir, spl, idx, idx)
-        line = "%d\t%s\t%s\n" % (idx, img_path, seg_path.replace('instanceIds', 'labelTrainIds'))
-        print(line)
-        files[spl].write(line)
 
-        ########## dump images #################
-        im_inp = read_pgm_xyz(inp)
-        im_centroids = load_seg(outp)
-        mask = get_labels(im_centroids)
-        mask = encode_classid(mask)
-        img_full_path = '%s/%s' % (root_path, img_path)
-        seg_full_path = '%s/%s' % (root_path, seg_path)
-        try:
-            #print("????:%s/%s" % (root_path, img_path))
-            pil_imsave(img_full_path, im_inp)
-            #print("????:%s/%s" % (root_path, img_path))
-        except IOError:
-            os.makedirs(os.path.dirname(img_full_path))
-            pil_imsave(img_full_path, im_inp)
-        try:
-            pil_imsave(seg_full_path, mask)
-        except IOError:
-            os.makedirs(os.path.dirname(seg_full_path))
-            pil_imsave(seg_full_path, mask)
-        ########## dump images #################
-    for v in files.values():
-        v.close()
-
-
+    len_ids = len(ids_)
+    workers = {}
+    for spl in splits_dict.values():
+        workers['train'] = lambda i: worker(i, spl, img_dir, gt_dir, files)
+    pool.imap(worker, ids_[:splits[0] * len_ids])
+    pool.imap(worker, ids_[splits[0] * len_ids: (splits[0] + splits[1])* len_ids])
+    pool.imap(worker, ids_[(splits[0] + splits[1])* len_ids:])
+    pool.close()
+    pool.join()
 
 if __name__ == "__main__":
     main()
